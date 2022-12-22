ea6d116afe726f6574c68aee093051ba8d0ce51b
Adam Durrant <adamdurrant92@gmail.com>
2022-12-22 21:22:02 +0000

    amended image to show poster image, added description and added rating

--- src/Movie.js
+++ src/Movie.js
@@ -8,14 +8,20 @@ export default function Movie({ movie }) {
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
     >
+      <div className='image'>
+        <img
+          src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
+          alt=''
+        />
+      </div>
       <div className='heading'>
         <h2>{movie.title}</h2>
       </div>
-      <div className='image'>
-        <img
-          src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path}
-          alt=''
-        />
+      <div className='description'>
+        <p>{movie.overview}</p>
+        <div className='rating'>
+          <p>Rating: {movie.vote_average}/10</p>
+        </div>
       </div>
     </motion.div>
   );
